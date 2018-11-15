fix forward-backward algorithm, compute gradients of parameters

crf.py
@@ -8,6 +8,7 @@ class CRF(object):
                 classes):
         self.input_size = input_size
         self.classes = classes
+        self.ready()
  
     def ready(self):
         self.alpha = np.zeros((0, 0))
@@ -23,7 +24,7 @@ class CRF(object):
                     np.zeros((self.input_size, self.classes)),
                     np.zeros((self.input_size, self.classes))]
 
-        self.db = [np.zeros((self.classes))]
+        self.db = np.zeros((self.classes))
         self.dw_edge = np.zeros((self.classes, self.classes))
 
 
@@ -37,68 +38,73 @@ class CRF(object):
         self.target_onehot[np.arange(self.seq_length), target] = 1
 
         self.alpha = np.zeros((self.classes, self.seq_length))
-        self.beta = np.zeros((self.classes, self.seq_length + 1))
+        self.beta = np.zeros((self.classes, self.seq_length))
 
         s = 0
         s = s + np.matmul(self.target_onehot[0], np.matmul(input[0], self.w[0]) + np.matmul(input[1], self.w[2]) + self.b)
 
         for i in range(1, self.seq_length - 1):
-            s = s + np.matmul(self.target_onehot[i], np.matmul(input[i], self.w[0]) + np.matmul(input[i-1], self.w[1]) + np.matmul(input[i+1], self.w[2]) + self.b) + np.matmul(np.matmul(self.target_onehot[i-1], self.w_edge), self.target_onehot[i])
+            s = s + np.matmul(self.target_onehot[i], np.matmul(input[i], self.w[0]) + np.matmul(input[i-1], self.w[1]) + np.matmul(input[i+1], self.w[2]) + self.b) +\
+                    np.matmul(np.matmul(self.target_onehot[i-1], self.w_edge), self.target_onehot[i])
         
         # i = seq_length - 1
         s = s + np.matmul(self.target_onehot[self.seq_length - 1], np.matmul(input[self.seq_length - 1], self.w[0]) + np.matmul(input[self.seq_length - 2], self.w[1]) + self.b) + np.matmul(np.matmul(self.target_onehot[self.seq_length - 2], self.w_edge), self.target_onehot[self.seq_length - 1])
 
+        self.omega = np.zeros((self.classes, self.classes, self.seq_length))
+        for s1 in range(self.classes):
+            for s2 in range(self.classes):
+                for j in range(1, self.seq_length - 1):
+                    xw = np.matmul(input[j], self.w[0]) + np.matmul(input[j-1], self.w[1]) + np.matmul(input[j+1], self.w[2]) + self.b
+                    self.omega[s1, s2, j] = np.exp(self.w_edge[s1, s2] + xw[s2])
+
+                xw = np.matmul(input[self.seq_length-1], self.w[0]) + np.matmul(input[self.seq_length-2], self.w[1]) + self.b
+                self.omega[s1, s2, self.seq_length - 1] = np.exp(self.w_edge[s1, s2] + xw[s2])
+
         # shape: (self.classes, ) = (input_size, )*(input_size, classes)
-        self.alpha[:, 0] = np.exp(np.matmul(input[0], self.w[0]) + np.matmul(input[1], self.w[2]))
+        self.alpha[:, 0] = np.exp(np.matmul(input[0], self.w[0]) + np.matmul(input[1], self.w[2]) + self.b)
+        for i in range(1, self.seq_length):
+            self.alpha[:, i] = np.matmul(self.alpha[:, i-1], self.omega[:, :, i])
 
-        # shape: (self.classes)
-        for i in range(1, self.seq_length-1):
-            #clique_potential = np.matmul
-            xw = (np.matmul(input[i], self.w[0]) + np.matmul(input[i+1], self.w[2]) + np.matmul(input[i-1], self.w[1]))
-            unary = xw[:, None]*self.target_onehot[i][None, :]
-            clique_potential = unary + self.w_edge
-            self.alpha[:, i] = np.matmul(np.exp(clique_potential), self.alpha[:, i-1])
+        self.beta[:, self.seq_length-1] = 1
+        for i in range(self.seq_length-2, -1, -1):
+            self.beta[:, i] = np.matmul(self.omega[:, :, i+1], self.beta[:, i+1])
 
-        xw = unary = (np.matmul(input[i], self.w[0]) + np.matmul(input[i+1], self.w[2]) + np.matmul(input[i-1], self.w[1]))
-        unary = xw[:, None]*self.target_onehot[self.seq_length-1]
-        clique_potential = unary + self.w_edge
-        self.alpha[:, self.seq_length-1] = np.matmul(np.exp(clique_potential), self.alpha[:, self.seq_length-2]) 
-
-        #for i in range(1, self.seq_length):
-        #    for j in range(self.classes):
-        #        self.alpha[j, i] = self.alpha[0, i-1]*np.exp(np.matmul(
-
-        self.beta[:, self.seq_length - 1] = 1
-        xw = (np.matmul(input[self.seq_length-1], self.w[0]) + np.matmul(input[self.seq_length-2], self.w[1]))
-        unary = xw[:, None]*self.target_onehot[self.seq_length - 1][None, :]
-        clique_potenial = unary + xw
-        self.beta[:, self.seq_length -2] = np.matmul(self.beta[:, self.seq_length-1], np.exp(clique_potential.T))
-        for i in range(self.seq_length - 3, -1, -1):
-            xw = (np.matmul(input[i+1], self.w[0]) + np.matmul(input[i+2], self.w[2]) + np.matmul(input[i], self.w[1]))
-            unary = xw[:, None]*self.target_onehot[i+1][None, :]
-
-            clique_potenial = unary + xw
-            self.beta[:, i] = np.matmul(self.beta[:, i+1], np.exp(clique_potential.T))
-
-        # compute loss
-        Z = np.sum(self.alpha[:, -1])
-        loss = Z - s
+        self.Z = np.sum(self.alpha[:, -1])
+        self.logZ = np.log(self.Z)
+        loss = self.logZ -s
         return loss
 
 
     def backward(self, input, target):
-        # dw: (input_size, classes)
-        # input: (seq_length, input_size)
-        # self.target_onehot: (seq_length, classes)
-        
         # shape: (input_size, classes)
-        np.matmul(input.T, self.target_onehot)
+        marginal_prob = np.zeros((self.seq_length, self.classes, self.classes))
+        for i in range(self.seq_length-1):
+            for s1 in range(self.classes):
+                for s2 in range(self.classes):
+                    marginal_prob[i, s1, s2] = self.alpha[s1, i] * self.omega[s1, s2, i+1] * self.beta[s2, i+1]
 
-        # shape: (input_size, classes)
+        marginal_prob = marginal_prob / self.Z
+        marginal_uni_prob = self.alpha*self.beta / self.Z
+
         for i in range(self.seq_length):
-            for j in range(self.classes):
-                self.dw[i, j]=None
-        self.dw_edge = None
+            self.dw[0] -= np.matmul(input[i, None].T, self.target_onehot[i, None] - marginal_uni_prob[:, i][None, :])
+
+        for i in range(self.seq_length-1):
+            self.dw[1] -= np.matmul(input[i, None].T, self.target_onehot[i + 1, None] - marginal_uni_prob[:, i+1][None, :])
+
+        for i in range(1, self.seq_length):
+            self.dw[2] -= np.matmul(input[i, None].T, self.target_onehot[i - 1, None] - marginal_uni_prob[:, i-1][None, :])
+
+        for i in range(self.seq_length):
+            self.db -=  self.target_onehot[i] - marginal_uni_prob[:, i]
+
+        # TODO: calculate dw_edge
+        for i in range(self.seq_length-1):
+            f = np.zeros((self.classes, self.classes))
+            f[target[i], target[i+1]] = 1
+            self.dw_edge -= f - marginal_prob[i]
+
+        return self.dw, self.dw_edge, self.db
 
     def loss(self):
         pass

verify_grads.py
@@ -3,22 +3,6 @@ from crf import CRF
 
 np.random.seed(2018)
 
-def test_forward():
- 
-    model = CRF(input_size=10, classes=3)
-    example = (np.random.rand(4,10),np.array([0,1,1,2]))
-    input, target = example
-    epsilon=1e-6
-
-    model.w = [0.01*np.random.rand(model.input_size,model.classes),
-                       0.01*np.random.rand(model.input_size,model.classes),
-                        0.01*np.random.rand(model.input_size,model.classes)]
-    model.b = 0.01*np.random.rand(model.classes)
-    model.w_edge = 0.01*np.random.rand(model.classes,model.classes)
-        
-    loss = model.forward(input,target)
-    return loss 
-
 
 def verify_gradients():
  
@@ -52,7 +36,6 @@ def verify_gradients():
                 
                 emp_dw[h][i,j] = (a-b)/(2.*epsilon)
 
-
     print ('dw[-1] diff.:',np.sum(np.abs(model.dw[-1].ravel()-emp_dw[-1].ravel()))/model.w[-1].ravel().shape[0])
     print ('dw[0] diff.:',np.sum(np.abs(model.dw[0].ravel()-emp_dw[0].ravel()))/model.w[0].ravel().shape[0])
     print ('dw[1] diff.:',np.sum(np.abs(model.dw[1].ravel()-emp_dw[1].ravel()))/model.w[1].ravel().shape[0])
@@ -69,7 +52,6 @@ def verify_gradients():
             model.w_edge[i,j] += epsilon
                 
             emp_dw_edge[i,j] = (a-b)/(2.*epsilon)
-
 
     print ('dw_edge  diff.:',np.sum(np.abs(model.dw_edge.ravel()-emp_dw_edge.ravel()))/model.w_edge.ravel().shape[0])
 
@@ -89,5 +71,5 @@ def verify_gradients():
 
 
 if __name__ == "__main__":
-    # verify_gradients()
-    print(test_forward())
+    verify_gradients()
+